4886f958417816d956868b2a56598e58c424ab63
plotnikovart <32990434+plotnikovart@users.noreply.github.com>
2018-04-05 22:20:37 +0300

    Add files via upload

---
+++ Drawing.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EducatedMonkey
+{
+    class Drawing
+    {
+        Canvas canv;
+        //Line _arm1, _arm2, _arm3, _arm4, _leg1, _leg2;
+        Ellipse mark1, mark2, mark3;
+        Image arm1, arm3, arm2, arm4, leg1, leg2, head;
+
+        public Drawing(Canvas canv, double leg, double arm, double r)
+        {
+            this.canv = canv;
+            // Инициализация линий
+            //InitializeLine(ref _arm1);
+            //InitializeLine(ref _arm2);
+            //InitializeLine(ref _arm3);
+            //InitializeLine(ref _arm4);
+            //InitializeLine(ref _leg1);
+            //InitializeLine(ref _leg2);
+
+            // Инициализация меток
+            InitializeMark(ref mark1, r * 1.4);
+            InitializeMark(ref mark2, r * 1.4);
+            InitializeMark(ref mark3, r * 1.6);
+
+            // Инициализация изображений
+            InitializeImage(ref leg1, "Resources/leg1.png", leg - r * 0.5);
+            InitializeImage(ref leg2, "Resources/leg2.png", leg - r * 0.5);
+            InitializeImage(ref arm1, "Resources/arm1.png", arm);
+            InitializeImage(ref arm2, "Resources/arm2.png", arm);
+            InitializeImage(ref arm3, "Resources/arm3.png", arm + 10);
+            InitializeImage(ref arm4, "Resources/arm4.png", arm + 10);
+            InitializeImage(ref head, "Resources/head.png", arm * 0.7);
+        }
+
+        /// <summary>
+        /// Метод для рисования обезьянки
+        /// </summary>
+        /// <param name="m">Массив координат конечностей</param>
+        public void Draw(double[] m)
+        {
+            // Добавление линий
+            //AddLine(_leg1, m[0], m[2], m[3], m[4]);
+            //AddLine(_leg2, m[1], m[2], m[3], m[4]);
+            //AddLine(_arm1, m[7], m[8], m[3], m[4]);
+            //AddLine(_arm2, m[9], m[10], m[3], m[4]);
+            //AddLine(_arm3, m[7], m[8], m[5], m[6]);
+            //AddLine(_arm4, m[9], m[10], m[5], m[6]);
+
+            // Добавление меток
+            AddMark(mark1, m[0], m[2]);
+            AddMark(mark2, m[1], m[2]);
+            AddMark(mark3, m[5], m[6] + mark3.Height / 2);
+
+            // Левая верхняя рука (arm1)
+            arm1.SetValue(Canvas.TopProperty, m[4] + 60);
+            arm1.SetValue(Canvas.LeftProperty, m[3] - 114);
+            arm1.RenderTransformOrigin = new Point(1, 0);
+            arm1.RenderTransform = new RotateTransform(180 / Math.PI * Math.Atan((m[3] - m[7]) / (m[8] - m[4])));
+
+            // Правая верхняя рука (arm2)
+            arm2.SetValue(Canvas.TopProperty, m[4] + 60);
+            arm2.SetValue(Canvas.LeftProperty, m[3]);
+            arm2.RenderTransformOrigin = new Point(0, 0);
+            arm2.RenderTransform = new RotateTransform(180 / Math.PI * Math.Atan((m[3] - m[9]) / (m[10] - m[4])));
+
+            // Левая нижняя рука (arm3)
+            arm3.SetValue(Canvas.TopProperty, m[8] - 10);
+            arm3.SetValue(Canvas.LeftProperty, m[7] - 81.5625);
+            arm3.RenderTransformOrigin = new Point(1, 0);
+            arm3.RenderTransform = new RotateTransform(-180 / Math.PI * Math.Atan((m[5] - m[7]) / (m[6] - m[8])));
+
+            // Правая нижняя рука (arm4)
+            arm4.SetValue(Canvas.TopProperty, m[10] - 10);
+            arm4.SetValue(Canvas.LeftProperty, m[9]);
+            arm4.RenderTransformOrigin = new Point(0, 0);
+            arm4.RenderTransform = new RotateTransform(180 / Math.PI * Math.Atan((m[5] - m[9]) / (m[10] - m[6])));
+
+            // Левая нога (leg1)
+            leg1.SetValue(Canvas.TopProperty, m[4]);
+            leg1.SetValue(Canvas.LeftProperty, m[3] - 231 + 20);
+            leg1.RenderTransformOrigin = new Point(1, 0);
+            leg1.RenderTransform = new RotateTransform(180 / Math.PI * Math.Atan((m[3] - m[0]) / (m[2] - m[4])));
+
+            // Правая нога (leg2)
+            leg2.SetValue(Canvas.TopProperty, m[4]);
+            leg2.SetValue(Canvas.LeftProperty, m[3] - 20);
+            leg2.RenderTransformOrigin = new Point(0, 0);
+            leg2.RenderTransform = new RotateTransform(180 / Math.PI * Math.Atan((m[3] - m[1]) / (m[2] - m[4])));
+
+            // Голова (head)
+            head.SetValue(Canvas.TopProperty, m[4] - head.Height / 2 + 25);
+            head.SetValue(Canvas.LeftProperty, m[3] - 170.7);
+        }
+
+        /// <summary>
+        /// Инициализация линий
+        /// </summary>
+        void InitializeLine(ref Line line)
+        {
+            line = new Line();
+            line.StrokeThickness = 4;
+            line.Stroke = Brushes.LightSteelBlue;
+            canv.Children.Add(line);
+        }
+
+        /// <summary>
+        /// Добавление линий
+        /// </summary>
+        void AddLine(Line line, double x1, double y1, double x2, double y2)
+        {
+            line.X1 = x1;
+            line.X2 = x2;
+            line.Y1 = y1;
+            line.Y2 = y2;
+        }
+
+        /// <summary>
+        /// Инициализация меток
+        /// </summary>
+        /// <param name="diam">Диаметр эллипса</param>
+        void InitializeMark(ref Ellipse e, double diam)
+        {
+            e = new Ellipse();
+            e.Height = e.Width = diam;
+            e.Stroke = Brushes.Gray;
+            e.StrokeThickness = 3;
+            e.Fill = Brushes.LightSkyBlue;
+            e.Opacity = 0.5;
+            canv.Children.Add(e);
+        }
+
+        /// <summary>
+        /// Добавление метки
+        /// </summary>
+        void AddMark(Ellipse e, double x, double y)
+        {
+            e.SetValue(Canvas.TopProperty, y - e.Height / 2);
+            e.SetValue(Canvas.LeftProperty, x - e.Width / 2);
+        }
+
+        /// <summary>
+        /// Инициализация изображения
+        /// </summary>
+        /// <param name="image">Картинка</param>
+        /// <param name="source">Путь</param>
+        /// <param name="len">Высота картинки</param>
+        void InitializeImage(ref Image image, string source, double len)
+        {
+            image = new Image();
+            image.Source = new BitmapImage(new Uri(source, UriKind.Relative));
+            image.Height = len;
+            canv.Children.Add(image);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Media;
+
+namespace EducatedMonkey
+{
+    public partial class MainWindow : Window
+    {
+        int n = 10;                                 // количество цифр в таблице умножения
+        double diam, space;                         // диаметр и расстояние между кругами
+        double x1, x2, y;                           // текущее положение ног
+        bool leftfoot = false, rightfoot = false;   // Левая и правая нога
+
+        Drawing draw;
+        Coordinates coord;
+        SoundPlayer player;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Initialization(n);
+
+            player = new SoundPlayer();
+            player.Stream = Properties.Resources.Click;
+        }
+
+        /// <summary>
+        /// События нажатия на изображение увеличения чисел в таблице
+        /// </summary>
+        private void arrowUp_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (n == 15)
+                return;
+
+            canv.Children.Clear();
+            player.Play();
+            Initialization(++n);
+            label.Content = n.ToString();
+        }
+
+        /// <summary>
+        /// События нажатия на изображение увеличения чисел в таблице
+        /// </summary>
+        private void arrowDown_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (n == 4)
+                return;
+
+            canv.Children.Clear();
+            player.Play();
+            Initialization(--n);
+            label.Content = n.ToString();
+        }
+
+        /// <summary>
+        /// Отрисовка поля
+        /// </summary>
+        /// <param name="n">Количество цифр</param>
+        void Initialization(int n)
+        {
+            Ellipse e;
+            Border bord;
+            TextBlock text;
+            diam = 2 * canv.Width / (3 * n - 1);
+            space = diam / 2;
+            for (int i = 0; i < n; i++)
+            {
+                // Добавление кругов
+                e = new Ellipse();
+                e.Height = e.Width = diam;
+                e.Stroke = Brushes.SaddleBrown;
+                e.Fill = Brushes.AliceBlue;
+                e.StrokeThickness = 3;
+                e.SetValue(Canvas.TopProperty, (canv.Height - diam));
+                e.SetValue(Canvas.LeftProperty, (i * (diam + space)));
+                canv.Children.Add(e);
+
+                // Добавление текста в круги
+                bord = new Border();
+                bord.Height = diam;
+                bord.Width = diam;
+                text = new TextBlock();
+                text.Text = (i + 1).ToString();
+                text.FontSize = diam * 0.5;
+                text.FontFamily = new FontFamily("Rockwell");
+                text.Foreground = Brushes.DarkBlue;
+                text.VerticalAlignment = VerticalAlignment.Center;
+                text.HorizontalAlignment = HorizontalAlignment.Center;
+                bord.Child = text;
+                bord.SetValue(Canvas.TopProperty, (canv.Height - diam));
+                bord.SetValue(Canvas.LeftProperty, (i * (diam + space)));
+                canv.Children.Add(bord);
+            }
+
+            // Отрисовка поля
+            y = canv.Height - diam / 2;
+            coord = new Coordinates(canv.Height, y);
+            double dim = 0.8 * diam;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    coord.Calc(i * (diam + space) + diam / 2, j * (diam + space) + diam / 2);
+                    double xa = coord.GetArms[0];
+                    double ya = coord.GetArms[1];
+
+                    // Добавление кругов
+                    e = new Ellipse();
+                    e.Width = e.Height = dim;
+                    e.Fill = Brushes.AliceBlue;
+                    e.Stroke = Brushes.DarkGreen;
+                    e.StrokeThickness = 3;
+                    e.SetValue(Canvas.TopProperty, ya);
+                    e.SetValue(Canvas.LeftProperty, xa - dim / 2);
+                    canv.Children.Add(e);
+
+                    // Добавление текста в круги
+                    bord = new Border();
+                    bord.Height = dim;
+                    bord.Width = dim;
+                    text = new TextBlock();
+                    text.Text = ((i + 1) * (j + 1)).ToString();
+                    text.FontSize = dim * 0.5;
+                    text.FontFamily = new FontFamily("Rockwell");
+                    text.Foreground = Brushes.DarkBlue;
+                    text.VerticalAlignment = VerticalAlignment.Center;
+                    text.HorizontalAlignment = HorizontalAlignment.Center;
+                    bord.Child = text;
+                    bord.SetValue(Canvas.TopProperty, ya);
+                    bord.SetValue(Canvas.LeftProperty, xa - dim / 2);
+                    canv.Children.Add(bord);
+                }
+            }
+
+            // Начальное положение обезьянки
+            x1 = diam / 2;
+            x2 = (n - 1) * (diam + space) + diam / 2;
+            coord.Calc(x1, x2);
+            draw = new Drawing(canv, coord.LenLeg, coord.LenArm, diam / 2);
+            draw.Draw(coord.GetCord);
+        }
+
+        /// <summary>
+        /// Событие нажатия кнопки
+        /// </summary>
+        private void MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Текущие координаты
+            double _x = e.GetPosition(canv).X;
+            double _y = e.GetPosition(canv).Y;
+
+            if (Math.Sqrt((_x - x1) * (_x - x1) + (_y - y) * (_y - y)) <= diam / 2)
+            {
+                leftfoot = true;
+            }
+            else if (Math.Sqrt((_x - x2) * (_x - x2) + (_y - y) * (_y - y)) <= diam / 2)
+            {
+                rightfoot = true;
+            }
+        }
+
+        /// <summary>
+        /// Событие движения мыши
+        /// </summary>
+        private void MouseMove(object sender, MouseEventArgs e)
+        {
+            if (leftfoot)
+            {
+                if (e.GetPosition(pole).X >= x2 - diam - space)
+                    return;
+
+                x1 = e.GetPosition(pole).X;
+                coord.Calc(x1, x2);
+                draw.Draw(coord.GetCord);
+            }
+
+            if (rightfoot)
+            {
+                if (x1 >= e.GetPosition(pole).X - diam - space)
+                    return;
+
+                x2 = e.GetPosition(pole).X;
+                coord.Calc(x1, x2);
+                draw.Draw(coord.GetCord);
+            }
+        }
+
+        /// <summary>
+        /// Событие отпуска кнопки
+        /// </summary>
+        private void MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (leftfoot)
+            {
+                leftfoot = false;
+                double x = e.GetPosition(pole).X;
+
+                if (x >= x2 - diam - space)
+                {
+                    player.Play();
+                    return;
+                }
+
+                if (Math.Abs(x % (diam + space) - diam / 2) <= diam / 2)
+                {
+                    player.Play();
+                    int i = (int)(x / (diam + space));
+                    x1 = i * (diam + space) + diam / 2;
+                    coord.Calc(x1, x2);
+                    draw.Draw(coord.GetCord);
+                }
+            }
+
+            if (rightfoot)
+            {
+                rightfoot = false;
+                double x = e.GetPosition(pole).X;
+
+                if (x1 >= x - diam - space)
+                {
+                    player.Play();
+                    return;
+                }
+
+                if (Math.Abs(x % (diam + space) - diam / 2) <= diam / 2)
+                {
+                    player.Play();
+                    int i = (int)(x / (diam + space));
+                    x2 = i * (diam + space) + diam / 2;
+                    coord.Calc(x1, x2);
+                    draw.Draw(coord.GetCord);
+                }
+            }
+        }
+    }
+}
