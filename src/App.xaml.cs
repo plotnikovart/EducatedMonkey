b96eec38216596688be06773ef9a3f1bd389aac5
Artem <artpl98@mail.ru>
2018-06-07 17:41:11 +0300

    Загрузка проекта

---
+++ src/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace EducatedMonkey
+{
+    /// <summary>
+    /// Точка входа в приложение
+    /// </summary>
+    public partial class App : Application
+    {
+        App()
+        {
+            // Запуск главного окна
+            Run(new MainWindow());
+        }
+    }
+}
